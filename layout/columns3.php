c4ff8f489669b3abfe810f345785f65332b048ae
malinz <m.hay@outlook.com>
2016-12-07 15:56:24 +1300

    Add custom block regions

--- layout/columns3.php
+++ layout/columns3.php
@@ -32,17 +32,32 @@
 $html = theme_thinredline_get_html_for_settings($OUTPUT, $PAGE);
 
 // Set default (LTR) layout mark-up for a three column page.
-$regionmainbox = 'span9 desktop-first-column';
+$topfullwidth = 'span12';
+$regionmainbox = 'span9';
+$upperfullwidth = 'span12';
 $regionmain = 'span8 pull-right';
 $sidepre = 'span4 desktop-first-column';
 $sidepost = 'span3 pull-right';
+$lowerfullwidth = 'span12';
+$bottomfullwidth = 'span12';
 // Reset layout mark-up for RTL languages.
 if (right_to_left()) {
-    $regionmainbox = 'span9 pull-right';
+    $topfullwidth = 'span12';
+	$regionmainbox = 'span9 pull-right';
+    $upperfullwidth = 'span12';
     $regionmain = 'span8';
     $sidepre = 'span4 pull-right';
     $sidepost = 'span3 desktop-first-column';
+	$lowerfullwidth = 'span12';
+	$bottomfullwidth = 'span12';
 }
+
+//Checks to see if there is content to display
+$hastopfullwidth = $PAGE->blocks->region_has_content('top-fullwidth', $OUTPUT);
+$hasbottomfullwidth = $PAGE->blocks->region_has_content('bottom-fullwidth', $OUTPUT);
+$hasupperfullwidth = $PAGE->blocks->region_has_content('upper-fullwidth', $OUTPUT);
+$haslowerfullwidth = $PAGE->blocks->region_has_content('lower-fullwidth', $OUTPUT);
+
 
 echo $OUTPUT->doctype() ?>
 <html <?php echo $OUTPUT->htmlattributes(); ?>>
@@ -77,21 +92,42 @@ echo $OUTPUT->doctype() ?>
 <div id="page" class="container-fluid">
     <?php echo $OUTPUT->full_header(); ?>
     <div id="page-content" class="row-fluid">
+        <?php
+		if ($hastopfullwidth)  {
+			echo $OUTPUT->blocks('top-fullwidth', $topfullwidth);
+		}
+		?>
         <div id="region-main-box" class="<?php echo $regionmainbox; ?>">
             <div class="row-fluid">
                 <section id="region-main" class="<?php echo $regionmain; ?>">
+                   <?php
+					if ($hasupperfullwidth)  {
+						echo $OUTPUT->blocks('upper-fullwidth', $upperfullwidth); 
+					}
+					?>
                     <?php
                     echo $OUTPUT->course_content_header();
                     echo $OUTPUT->main_content();
                     echo $OUTPUT->course_content_footer();
                     ?>
+                    <?php
+                    if ($haslowerfullwidth)  {
+						echo $OUTPUT->blocks('lower-fullwidth', $lowerfullwidth); 
+					}
+					?>
                 </section>
                 <?php echo $OUTPUT->blocks('side-pre', $sidepre); ?>
             </div>
         </div>
         <?php echo $OUTPUT->blocks('side-post', $sidepost); ?>
     </div>
-
+    <div class="row-fluid">
+     <?php
+	if ($hasbottomfullwidth)  {
+		echo $OUTPUT->blocks('bottom-fullwidth', $bottomfullwidth); 
+	}
+	?>
+	</div>
     <footer id="page-footer">
         <div id="course-footer"><?php echo $OUTPUT->course_footer(); ?></div>
         <p class="helplink"><?php echo $OUTPUT->page_doc_link(); ?></p>
